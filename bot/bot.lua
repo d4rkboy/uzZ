bf062864d6f8a7edbae2b9f588d9c3338d0fbc6d
terinux <aryangithub@gmail.com>
2016-03-15 13:02:33 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "youtube",
       "media_handler",
       "moderation"},
-    sudo_users = { V_for_Vendetta , 176282406 },
+    sudo_users = { V_for_Vendetta , 115740444 },
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'}
   }
